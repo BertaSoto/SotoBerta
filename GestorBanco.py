ad9822d5b1812d9f553dc5dcfbcc3522389e8b9a
BertaSoto <bert.soto@duocuc.cl>
2024-06-26 18:48:24 -0400

    Ingreso clientes y saldos

---
+++ GestorBanco.py
@@ -0,0 +1,37 @@
+import statistics
+import math
+import csv
+
+clientes_banco = [
+   { "Nombre": "Alma",
+    "Saldo": 200000,
+    
+    "Nombre": "Bernardo",
+    "Saldo": 1500000,
+    
+    "Nombre": "Claudio",
+    "Saldo": 10000,
+    
+    "Nombre": "Diego",
+    "Saldo": 500000,
+    
+    "Nombre": "Esperanza",
+    "Saldo": 3000000,
+    
+    "Nombre": "Francisca",
+    "Saldo": 200000,
+    
+    "Nombre": "Gabriel",
+    "Saldo": 40000000,
+    
+    "Nombre": "Hector",
+    "Saldo": 100,
+    
+    "Nombre": "Ines",
+    "Saldo": 60000,
+    
+    "Nombre": "Javier",
+    "Saldo": 600000
+   }
+]
+
